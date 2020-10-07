c0c03c5da55f86fd50d1538d682cc44a920342ba
lily <lily_ferreira@yahoo.com>
2020-10-07 10:03:02 -0700

    updated script and tested

--- bash/improveddice.sh
+++ bash/improveddice.sh
@@ -7,7 +7,8 @@
 #  put the number of sides in a variable which is used as the range for the random number
 #  put the bias, or minimum value for the generated number in another variable
 #  roll the dice using the variables for the range and bias i.e. RANDOM % range + bias
-
+sides=$((6))
+bias=$((1))
 # Task 2:
 #  generate the sum of the dice
 #  generate the average of the dice
@@ -17,7 +18,15 @@
 # Tell the user we have started processing
 echo "Rolling..."
 # roll the dice and save the results
-die1=$(( RANDOM % 6 + 1))
-die2=$(( RANDOM % 6 + 1 ))
+die1=$(( RANDOM % $sides + $bias ))
+die2=$(( RANDOM % $sides + $bias ))
+sum=$((die1 + die2))
+average=$(($sum/2))
 # display the results
 echo "Rolled $die1, $die2"
+
+#generate sum of directories
+echo "The sum of the rolled die is $sum."
+
+# generate the average of the dice
+echo "The average of the rolled die is $average. "
