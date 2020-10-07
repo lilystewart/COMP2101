5200eecef49d592e9cdda4f8a683f2950619db22
lily <lily_ferreira@yahoo.com>
2020-10-07 11:01:27 -0700

    Arithmetic script Updated and tested

--- bash/arithmetic-demo.sh
+++ bash/arithmetic-demo.sh
@@ -3,18 +3,16 @@
 # this script demonstrates doing arithmetic
 
 # Task 1: Remove the assignments of numbers to the first and second number variables. Use one or more read commands to get 3 numbers from the user.
+prompt="Please enter 3 numbers of your choice: "
+read -p "$prompt" firstnum secondnum thirdnum
+
 # Task 2: Change the output to only show:
 #    the sum of the 3 numbers with a label
 #    the product of the 3 numbers with a label
-
-firstnum=5
-secondnum=2
-sum=$((firstnum + secondnum))
-dividend=$((firstnum / secondnum))
-fpdividend=$(awk "BEGIN{printf \"%.2f\", $firstnum/$secondnum}")
+sum=$((firstnum + secondnum + thirdnum))
+product=$((firstnum * secondnum * thirdnum))
 
 cat <<EOF
-$firstnum plus $secondnum is $sum
-$firstnum divided by $secondnum is $dividend
-  - More precisely, it is $fpdividend
+$firstnum plus $secondnum plus $thirdnum is $sum
+$firstnum multiplied by $secondnum multiplied by $thirdnum is $product
 EOF
