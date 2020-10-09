f17d426976adb9defe2bb90e8bb670919e0391fc
lily <lily_ferreira@yahoo.com>
2020-10-09 12:01:03 -0700

    Updated and tested

--- bash/hostname-changer.sh
+++ bash/hostname-changer.sh
@@ -6,12 +6,16 @@
 # Send your script to your github repo, and submit the URL with screenshot on Blackboard
 
 # Get the current hostname using the hostname command and save it in a variable
+currenthost=$(hostname)
 
 # Tell the user what the current hostname is in a human friendly way
+echo "Your current hostname is $currenthost."
 
 # Ask for the user's student number using the read command
+read -p "Please enter your student number? " studentnum
 
 # Use that to save the desired hostname of pcNNNNNNNNNN in a variable, where NNNNNNNNN is the student number entered by the user
+newhostname=pc$studentnum
 
 # If that hostname is not already in the /etc/hosts file, change the old hostname in that file to the new name using sed or something similar and
 #     tell the user you did that
@@ -20,3 +24,14 @@
 # If that hostname is not the current hostname, change it using the hostnamectl command and
 #     tell the user you changed the current hostname and they should reboot to make sure the new name takes full effect
 #e.g. hostnamectl set-hostname $newname
+
+sed -i "s/$currenthost/$newhostname/w changes.txt" /etc/hosts
+if [ -s changes.txt ]; then # if a file exists and has a file size greater than 0
+  echo "Changed hostname from: $currenthost to: $newhostname."
+  hostnamectl set-hostname $newhostname
+  echo "Please reboot your PC now to save these changes."
+else
+  echo "No changes to hostname have been made."
+fi
+
+rm changes.txt
--- bash/testfilehostname.sh
+++ bash/testfilehostname.sh
@@ -11,12 +11,12 @@ echo "Your new hostname is going to be $newhostname."
 
 # find currenthost name and replace with newhostname in testfile, write changes to changes.txt
 sed -i "s/$currenthost/$newhostname/w changes.txt" testfile
-if [ -s changes.txt ]; then
+if [ -s changes.txt ]; then # if a file exists and has a file size greater than 0
   echo "Changed hostn from: $currenthost to: $newhostname."
-  #hostnamectl set-hostname $newhostname
+  #hostnamectl set-hostname $newhostname | do not want to execute this until testing finished.....
   echo "Please reboot your PC now to save changes."
 else
   echo "No changes made."
 fi
 
-rm changes.txt  
+#rm changes.txt | include as active code in production script hostname-changer.sh 
