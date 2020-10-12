82b55c0d8d2434b8022f7fb1d2d2273a8e25ca46
lily <lily_ferreira@yahoo.com>
2020-10-12 09:15:08 -0400

    Updates

--- bash/login-welcome.sh
+++ bash/login-welcome.sh
@@ -5,6 +5,8 @@
 #   Welcome to planet hostname, title name!
 
 # Task 1: Use the variable $USER instead of the myname variable to get your name
+
+
 # Task 2: Dynamically generate the value for your hostname variable using the hostname command - e.g. $(hostname)
 # Task 3: Add the time and day of the week to the welcome message using the format shown below
 #   Use a format like this:
@@ -21,29 +23,35 @@ myname=$USER
 hostname=$HOSTNAME
 day=$(date +%A)
 time=$(date +%T" "%p)
-#if statement for different titles depending on the day of the week.
+#if statement for different titles depending on the day of the week. Added daymessages for fun.
 title=""
+daymsg=""
 if [ $day == "Monday" ]; then
-  title="High Mage"
+  title="Moody"
+  daymsg="You look like you need an extra coffee today."
 elif [ $day == "Tuesday" ]; then
-  title="Royal Templar"
+  title="Chirpy"
+  daymsg="Well you look ready to do some work!"
 elif [ $day == "Wednesday" ]; then
-  title="Prime Justicar"
+  daymsg="There is lots to do today! Roll up those sleeves."
+  title="Ambitious"
 elif [ $day == "Thursday" ]; then
-  title="Archwarrior"
+  title="Grumpy"
+  daymsg="You look a little worn out. Hang in there!"
 elif [ $day == "Friday" ]; then
-  title="Alpha"
+  title="Joyful"
+  daymsg="You made it to the end of the week and no one is hurt!!"
 elif [ $day == "Saturday" ]; then
-  title="Sentinel"
+  title="Overachiever"
+  daymsg="Shouldn't you be sleeping in and enjoying the day?"
 else
-  title="High Sage"
+  title="Workaholic"
+  daymsg="Seriously.... why are you here?"
 fi
 ###############
 # Main        #
 ###############
-cat <<EOF
-
-Welcome to Planet $hostname, $title $myname!
+welcome="Welcome to Planet $hostname, $title $myname!
 It is $day at $time.
-
-EOF
+$daymsg."
+echo $welcome|cowsay
