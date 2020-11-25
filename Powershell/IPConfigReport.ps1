46f5c28bcff49e6f8e98915617c24adfd5bfe857
lilystewart <lilystewart@users.noreply.github.com>
2020-11-25 15:16:18 -0500

    Create IPConfigReport.ps1
    ip config Final

---
+++ Powershell/IPConfigReport.ps1
@@ -0,0 +1,5 @@
+﻿# IP Configuration Report 
+
+Get-CimInstance -class win32_networkadapterconfiguration  | 
+Where-Object {$_.ipenabled -eq "True"} | 
+Format-Table Description,Index,IpAddress,IpSubnet,DnsDomain,DnsServerSearchOrder
